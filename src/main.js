b29b6f207351fcd9efb964f6dbe335e3236ae7ef
이찬호 <0407chan@naver.com>
2021-08-29 14:37:39 +0900

    chore. remove console.log

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import { default as invoices } from './invoices.json'
 import { default as plays } from './plays.json'
 import { statement } from './statement.js'
-
-console.log('\n-----실행결과-----')
+console.log()
 for (let invoice of invoices) {
   const result = statement(invoice, plays)
   console.log(result)
 }
-console.log('-----종료-----')
