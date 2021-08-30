d528214d1dfed94944c326d4cc200d9f378aef60
이찬호 <chanho@woowahan.com>
2021-08-30 21:19:23 +0900

    [32] amountFor 함수 nest함수로 추출

--- src/statement.js
+++ src/statement.js
@@ -10,6 +10,26 @@ export function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID]
+    let thisAmount = amountFor(perf, play)
+
+    //포인트 적립
+    volumnCredits += Math.max(perf.audience - 30, 0)
+    //희극 관객 5명마다 추가 포인트 제공
+    if ('comedy' === play.type) {
+      volumnCredits += Math.floor(perf.audience / 5)
+    }
+
+    //청구 내역 출력
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    }석)\n`
+    totalAmount += thisAmount
+  }
+  result += `총액: ${format(totalAmount / 100)}\n`
+  result += `적립 포인트: ${volumnCredits}점\n`
+  return result
+
+  function amountFor(perf, play) {
     let thisAmount = 0
 
     switch (play.type) {
@@ -30,20 +50,6 @@ export function statement(invoice, plays) {
         throw new Error(`알 수 없는 장르: ${play.type}`)
     }
 
-    //포인트 적립
-    volumnCredits += Math.max(perf.audience - 30, 0)
-    //희극 관객 5명마다 추가 포인트 제공
-    if ('comedy' === play.type) {
-      volumnCredits += Math.floor(perf.audience / 5)
-    }
-
-    //청구 내역 출력
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
-    }석)\n`
-    totalAmount += thisAmount
+    return thisAmount
   }
-  result += `총액: ${format(totalAmount / 100)}\n`
-  result += `적립 포인트: ${volumnCredits}점\n`
-  return result
 }
