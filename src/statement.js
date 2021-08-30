bc154948ad539f18ab0348bf736da5ef59de2549
이찬호 <chanho@woowahan.com>
2021-08-30 21:28:53 +0900

    [38] 필요없어진 매개 변수 제거

--- src/statement.js
+++ src/statement.js
@@ -9,7 +9,7 @@ export function statement(invoice, plays) {
   }).format
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf))
+    let thisAmount = amountFor(perf)
 
     //포인트 적립
     volumnCredits += Math.max(perf.audience - 30, 0)
@@ -28,7 +28,7 @@ export function statement(invoice, plays) {
   result += `적립 포인트: ${volumnCredits}점\n`
   return result
 
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0
 
     switch (playFor(aPerformance).type) {
