8d9c3f8fe79226ef5f17cd991b78db40f3c7be1e
이찬호 <chanho@woowahan.com>
2021-08-30 21:27:56 +0900

    [37][함수 선언 바꾸기] amountFor 함수안에있는 play를 playFor로 대체

--- src/statement.js
+++ src/statement.js
@@ -31,7 +31,7 @@ export function statement(invoice, plays) {
   function amountFor(aPerformance, play) {
     let result = 0
 
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case 'tragedy':
         result = 40000
         if (aPerformance.audience > 30) {
@@ -46,7 +46,7 @@ export function statement(invoice, plays) {
         result += 300 * aPerformance.audience
         break
       default:
-        throw new Error(`알 수 없는 장르: ${play.type}`)
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`)
     }
 
     return result
