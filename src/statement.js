8f8f4b29d998d34a90b68ae8d86e3efeb796a0fb
이찬호 <0407chan@naver.com>
2021-08-31 20:27:02 +0900

    [44][함수 선언 바꾸기] format -> usd

--- src/statement.js
+++ src/statement.js
@@ -7,21 +7,21 @@ export function statement(invoice, plays) {
     volumnCredits += volumnCreditsFor(perf)
 
     //청구 내역 출력
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`
     totalAmount += amountFor(perf)
   }
-  result += `총액: ${format(totalAmount / 100)}\n`
+  result += `총액: ${usd(totalAmount)}\n`
   result += `적립 포인트: ${volumnCredits}점\n`
   return result
 
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
       minimumFractionDigits: 2,
-    }).format(aNumber)
+    }).format(aNumber / 100)
   }
 
   function volumnCreditsFor(perf) {
