6dbc32f3d9cc1ba0e0b4cd78d4725bf9a1d954cd
이찬호 <chanho@woowahan.com>
2021-09-02 09:01:03 +0900

    [53] 데이터 구조 객체 생성 및 전달

--- src/statement.js
+++ src/statement.js
@@ -1,8 +1,9 @@
 export function statement(invoice, plays) {
-  return renderPlainText(invoice, plays)
+  const statementData = {}
+  return renderPlainText(statementData, invoice, plays)
 }
 
-export function renderPlainText(invoice, plays) {
+export function renderPlainText(data, invoice, plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`
   for (let perf of invoice.performances) {
     //청구 내역 출력
