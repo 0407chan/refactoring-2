1b590cce7c6ef6f3cb1374b406cba7c650ff4083
이찬호 <chanho@woowahan.com>
2021-09-02 09:00:08 +0900

    [52][함수 추출하기] renderPlainText 함수를 추출했다.

--- src/statement.js
+++ src/statement.js
@@ -1,4 +1,8 @@
 export function statement(invoice, plays) {
+  return renderPlainText(invoice, plays)
+}
+
+export function renderPlainText(invoice, plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`
   for (let perf of invoice.performances) {
     //청구 내역 출력
