1ca22e503d59379886634340435b7195c0a40492
이찬호 <chanho@woowahan.com>
2021-09-02 09:02:17 +0900

    [54] 고객정보를 중간 데이터로 옮김

--- src/statement.js
+++ src/statement.js
@@ -1,10 +1,11 @@
 export function statement(invoice, plays) {
   const statementData = {}
+  statementData.customer = invoice.customer
   return renderPlainText(statementData, invoice, plays)
 }
 
 export function renderPlainText(data, invoice, plays) {
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`
+  let result = `청구 내역 (고객명: ${data.customer})\n`
   for (let perf of invoice.performances) {
     //청구 내역 출력
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
