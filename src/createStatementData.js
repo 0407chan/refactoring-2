57095ed436812360035bf93ea92dc93d18d56356
이찬호 <chanho@woowahan.com>
2021-09-05 19:21:43 +0900

    [71][팩터리 함수로 바꾸기] 타입에 따라 서브 클래스를 호출 할 수 있게됐다.

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -1,5 +1,12 @@
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay)
+  switch (aPlay.type) {
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay)
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay)
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`)
+  }
 }
 
 class PerformanceCalculator {
@@ -40,6 +47,9 @@ class PerformanceCalculator {
     return result
   }
 }
+
+class TragedyCalculator extends PerformanceCalculator {}
+class ComedyCalculator extends PerformanceCalculator {}
 
 export default function createStatementData(invoice, plays) {
   const statementData = {}
