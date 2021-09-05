e10988ceb7f1f3354131d0d7348897c83b08839e
이찬호 <chanho@woowahan.com>
2021-09-05 18:47:54 +0900

    [60] html 렌더 코드 작성

--- src/statement.js
+++ src/statement.js
@@ -13,12 +13,26 @@ export function renderPlainText(data, plays) {
   result += `총액: ${usd(data.totalAmount)}\n`
   result += `적립 포인트: ${data.totalVolumnCredits}점\n`
   return result
+}
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100)
+export function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.currency})</h1>\n`
+  result += `<table>\n`
+  result += `<tr><th>연극</th><th>좌석 수</th><th>금액</th>`
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`
+    result += `<td>${usd(perf.amount)}</td></tr>\n`
   }
+  result += `</table>\n`
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`
+  result += `<p>적립 포인트: <em>${data.totalVolumnCredits}</em>점</p>\n`
+  return result
+}
+
+export function usd(aNumber) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100)
 }
